e7f5b1a52b358caf93a433e189d51fc068d9e2b2
Alexander-222-Mor <amorgunov948@gmail.com>
2024-11-04 13:22:52 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,19 @@
+my_dict = {'Andrey': 2012, 'Max': 2004, 'Sasha': 2000}
+print (my_dict)
+print (my_dict['Max'])
+(my_dict.update({'Misha': 2003,
+                 'Sisa':1999}))
+print (my_dict)
+my_dict.pop('Max')
+print (my_dict)
+print (my_dict.get('Bili'))
+
+my_set ={1, 1, 2, 2, 3, 4, 4, True, 'son','son'}
+print (my_set)
+print (my_set.add(7))
+print (my_set.add(9))
+print (my_set)
+print (my_set.discard(1))
+print (my_set)
+
+
